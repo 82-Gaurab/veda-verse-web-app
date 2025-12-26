5323c49f80f28053a6a10a053e4624c0fbfe8ff1
GAURAB <gaurabkhadka4082@gmail.com>
2025-12-26 18:13:13 +0545

    Add schema verification in login and register and handle error

--- app/(auth)/_components/login-form.tsx
+++ app/(auth)/_components/login-form.tsx
@@ -1,23 +1,56 @@
+"use client";
 import Link from "next/link";
+import {useForm} from "react-hook-form";
+
+import { LoginData, loginSchema } from "../schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 
 export default function LoginForm() {
+
+  const {register, handleSubmit, formState: { errors, isSubmitting}} = useForm<LoginData>(
+    {
+      resolver: zodResolver(loginSchema)
+    }
+  );
+
+
+  
+  const onSubmit = async (data: LoginData) => {
+    alert(data.email);
+  }
+
+
   return (
     <div className="mx-auto max-w-md-border m-2 p-4 rounded">
-      <div className="m-1">
-        <label className="block mb-2 font-semibold">Email:</label>
-        <input className="p-1 border" />
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="m-1">
+          <label className="block mb-2 font-semibold">Email:</label>
+          <input className="p-1 border" {...register("email")}/>
+            {
+            errors.email && // conditional rendering
+            <span className="text-red-500">
+              {errors.email.message}
+            </span>
+          }
+        </div>
 
-      <div className="m-1">
-        <label className="block mb-2 font-semibold">Password:</label>
-        <input className="p-1 border" />
-      </div>
-      <button className="bg-green-500 border rounded p-2 mt-2 text-white">Login</button>
-      <div>
-        Create an account. 
-        <Link href= "/register" className="text-blue-500 underline"> Register</Link> 
-      </div>
+        <div className="m-1">
+          <label className="block mb-2 font-semibold">Password:</label>
+          <input className="p-1 border" {...register("password")}/>
+            {
+            errors.password && // conditional rendering
+            <span className="text-red-500">
+              {errors.password.message}
+            </span>
+          }
+        </div>
+        <button type="submit" className="bg-green-500 border rounded p-2 mt-2 text-white">Login</button>
+        <div>
+          Create an account. 
+          <Link href= "/register" className="text-blue-500 underline"> Register</Link> 
+        </div>
+      </form>
     </div>
   );
 }
--- app/(auth)/_components/register-form.tsx
+++ app/(auth)/_components/register-form.tsx
@@ -1,30 +1,62 @@
+"use client";
+
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { RegisterData, registerSchema } from "../schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function RegisterForm() {
+
+  const {register, handleSubmit, formState: {errors, isSubmitting}} = useForm<RegisterData>(
+    {
+      resolver: zodResolver(registerSchema)
+    }
+  );
+
+  
+  const onSubmit = async (data: RegisterData) => {
+    alert(`${data.name}, ${data.email}`);
+  }
+
+
   return (
     <div className="mx-auto max-w-md-border m-2 p-4 rounded">
-      <div className="m-1">
-        <label className="block mb-2 font-semibold">Full Name:</label>
-        <input className="p-1 border" />
-      </div>
-      <div className="m-1">
-        <label className="block mb-2 font-semibold">Email:</label>
-        <input className="p-1 border" />
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="m-1">
+          <label className="block mb-2 font-semibold">Full Name:</label>
+          <input className="p-1 border" {...register("name")}/>
+          {
+            errors.name && <span className="text-red-500">{errors.name.message}</span>
+          }
+        </div>
+        <div className="m-1">
+          <label className="block mb-2 font-semibold">Email:</label>
+          <input className="p-1 border" {...register("email")}/>
+          {
+            errors.email && <span className="text-red-500">{errors.email.message}</span>
+          }
+        </div>
 
-      <div className="m-1">
-        <label className="block mb-2 font-semibold">Password:</label>
-        <input className="p-1 border" />
-      </div>
-      <div className="m-1">
-        <label className="block mb-2 font-semibold">Confirm Password:</label>
-        <input className="p-1 border" />
-      </div>
-      <button className="bg-green-500 border rounded p-2 mt-2 text-white">Register</button>
-      <div>
-        Already have an account. 
-        <Link href= "/login" className="text-blue-500 underline">Login</Link> 
-      </div>
+        <div className="m-1">
+          <label className="block mb-2 font-semibold">Password:</label>
+          <input className="p-1 border" {...register("password")}/>
+          {
+            errors.password && <span className="text-red-500">{errors.password.message}</span>
+          }
+        </div>
+        <div className="m-1">
+          <label className="block mb-2 font-semibold">Confirm Password:</label>
+          <input className="p-1 border" {...register("confirmPassword")}/>
+          {
+            errors.confirmPassword && <span className="text-red-500">{errors.confirmPassword.message}</span>
+          }
+        </div>
+        <button type="submit" className="bg-green-500 border rounded p-2 mt-2 text-white">Register</button>
+        <div>
+          Already have an account. 
+          <Link href= "/login" className="text-blue-500 underline">Login</Link> 
+        </div>
+      </form>
     </div>
   );
 }
