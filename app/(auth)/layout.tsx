71ba2a91e86333e1ce272525ca40fe35ca504871
GAURAB <gaurabkhadka4082@gmail.com>
2025-12-27 15:00:20 +0545

    Remove the border

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <section className="flex flex-col border min-h-screen items-center bg-[url(/images/bg.jpg)] bg-cover bg-no-repeat bg-center">
+    <section className="flex flex-col min-h-screen items-center bg-[url(/images/bg.jpg)] bg-cover bg-no-repeat bg-center">
       <div className="flex flex-col gap-1.5 items-center mt-10" >
         <Image src="/icons/logo.png" height={130} width={130} alt="logo"/>
         {children}
