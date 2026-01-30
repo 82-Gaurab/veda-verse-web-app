3b9eda107fefe3f647459ab8052010f940fc2500
GAURAB <gaurabkhadka4082@gmail.com>
2026-01-30 20:07:24 +0545

    Add admin route and user profile update route

--- lib/api/endpoint.ts
+++ lib/api/endpoint.ts
@@ -4,5 +4,11 @@ export const API = {
   AUTH: {
     REGISTER: "/api/v1/auth/register",
     LOGIN: "/api/v1/auth/login",
+    UPDATEPROFILE: "/api/v1/auth/update-profile",
+  },
+  ADMIN: {
+    USER: {
+      CREATE: "/api/v1/admin/users/",
+    },
   },
 };
