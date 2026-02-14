b06d4c5b8300e8fc8ca5bb64dd43d4ae32ed55d0
GAURAB <gaurabkhadka4082@gmail.com>
2026-02-14 08:56:18 +0545

    image url

--- app/admin/users/_components/DisplayUserTable.tsx
+++ app/admin/users/_components/DisplayUserTable.tsx
@@ -201,9 +201,9 @@ const DisplayUserTable = ({
                 {user._id}
               </td>
               <td className="px-4 py-2">
-                {user.imageUrl ? (
-                  <Image
-                    src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${user.imageUrl}`}
+                {user.profilePicture ? (
+                  <img
+                    src={`${process.env.Next_PUBLIC_API_BASE_URL}${user.profilePicture}`}
                     alt="User Image"
                     className="w-10 h-10 rounded-full object-cover"
                     width={40}
